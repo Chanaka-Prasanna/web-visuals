b16393247ce992b6c213aa709480485de36213af
Chanaka-Prasanna <chanakapinfo@gmail.com>
2025-03-17 09:38:01 +0530

    modified pie chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import ShowPieChart from "@/components/charts/ShowPieChart";
-import { ModeToggle } from "@/components/toggle";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <ShowPieChart />
-      <ModeToggle />
     </main>
   );
 }
---
+++ components/shared/navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ModeToggle } from "../toggle";
+
+const navbar = () => {
+  return (
+    <div>
+      <ModeToggle />
+    </div>
+  );
+};
+
+export default navbar;
